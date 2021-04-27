18cb81f8847d2a682df59ab868d18d50423dd1cc
buerodigital <77230151+buerodigital@users.noreply.github.com>
2021-04-27 21:39:54 +0200

    Update 01_proxy.sh

--- compose/01_proxy.sh
+++ compose/01_proxy.sh
@@ -1,1 +1,47 @@
+version: "3.7"
+
+services:
+  proxy:
+    image: linuxserver/nginx:amd64-version-8349a582
+    container_name: proxy
+    depends_on:
+      - "heimdall"
+    environment:
+      # User (Docker User) und Gruppen (Docker Gruppe) anpassen
+      - PUID=1000
+      - PGID=1000
+      - TZ=Europe/Berlin
+    volumes:
+      - conf_01_proxy:/config
+    ports:
+      - 80:80
+      - 443:443
+    restart: unless-stopped
+    networks:
+      - proxy
+
+
+  heimdall:
+    image: linuxserver/heimdall:version-2.2.2
+    container_name: heimdall
+    environment:
+      - PUID=1000
+      - PGID=1000
+      - TZ=Europe/Berlin
+    volumes:
+      - conf_01_heimdall:/config
+    restart: unless-stopped
+    networks:
+      - proxy
+
+networks:
+  proxy:
+    external: true
+
+volumes:
+  conf_01_proxy:
+    external: true
+  conf_01_heimdall:
+    external: true
+
 
