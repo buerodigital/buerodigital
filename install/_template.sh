bde7314dd9097efc3ab181a86c448e6310e31da4
buerodigital <77230151+buerodigital@users.noreply.github.com>
2021-04-29 22:44:15 +0200

    Update _template.sh

--- install/_template.sh
+++ install/_template.sh
@@ -2,6 +2,89 @@
 
 function install {
   echo "Install"
+  
+docker network create proxy
+docker volume create --name=conf_01_proxy
+docker volume create --name=conf_01_heimdall
+  
+  cat > 01_proxy.yml <<EOL
+version: "3.7"
+
+services:
+  proxy:
+    image: linuxserver/nginx:amd64-version-8349a582
+    container_name: proxy
+    depends_on:
+      - "heimdall"
+    environment:
+      # User (Docker User) und Gruppen (Docker Gruppe) anpassen
+      - PUID=1000
+      - PGID=1000
+      - TZ=Europe/Berlin
+    volumes:
+      - conf_01_proxy:/config:z
+    ports:
+      - 80:80
+      - 443:443
+    restart: unless-stopped
+    networks:
+      - proxy
+
+
+  heimdall:
+    image: linuxserver/heimdall:version-2.2.2
+    container_name: heimdall
+    environment:
+      - PUID=1000
+      - PGID=1000
+      - TZ=Europe/Berlin
+    volumes:
+      - conf_01_heimdall:/config:z
+    restart: unless-stopped
+    networks:
+      - proxy
+
+networks:
+  proxy:
+    external: true
+
+volumes:
+  conf_01_proxy:
+    external: true
+  conf_01_heimdall:
+    external: true
+    
+  EOL
+  
+  CONFIRM="n"
+  while [ "$CONFIRM" == "n" ]
+  do
+    docker-compose -f 01_proxy.yml pull
+    echo -e "\nFalls die Installation fehlerhaft war bitte mit \"n\" wiederholen:\n"
+    read CONFIRM
+  done  
+
+docker-compose -f 01_proxy.yml up -d
+docker-compose -f 01_proxy.yml down
+
+sudo mkdir /var/lib/docker/volumes/conf_01_proxy/_data/nginx/proxy-confs
+
+  cat > default <<EOL
+      
+  EOL
+sudo mv default /var/lib/docker/volumes/conf_01_proxy/_data/nginx/site-confs/default
+
+  cat > proxy.conf <<EOL
+      
+  EOL
+sudo mv proxy.conf /var/lib/docker/volumes/conf_01_proxy/_data/nginx/proxy.conf
+
+  cat > heimdall.subfolder.conf <<EOL
+      
+  EOL
+sudo mv heimdall.subfolder.conf /var/lib/docker/volumes/conf_01_proxy/_data/nginx/proxy-confs/heimdall.subfolder.conf
+  
+  
 }
 
 function remove {
