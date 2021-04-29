28ad4484cfcc10df76aa06c5a84525a8f25c69eb
buerodigital <77230151+buerodigital@users.noreply.github.com>
2021-04-29 22:21:17 +0200

    Update _template.sh

--- install/_template.sh
+++ install/_template.sh
@@ -1,34 +1,31 @@
 #!/bin/bash
 
-APP="01_proxy"
-
-function install(){
-    echo "Install"
+function install {
+  echo "Install"
 }
 
-function start(){
-    echo "Start"
+function remove {
+  echo "Remove"
 }
 
-function stop(){
-    echo "Stop"
+function start {
+  echo "Start"
+  #docker-compose -f 01_proxy.yml up -d
 }
 
-function remove(){
-    echo "Remove"
-}
+function stop {
+  echo "Stop"
+  #docker-compose -f 01_proxy.yml down
+  }
 
-function about(){
-  #cat "https://github.com/buerodigital/nas/blob/main/_info/"$APP".md"
-}
-
-function help(){
-  echo "Die Funktion bitte mit einem der folgenden Parameter aufrufen:"
-  echo -e " -a --about\t\t Gibt Informationen über die Anwendung aus"     
-  echo -e " -i --install\t\t Installiert die Anwendung"
-  echo -e " -s --start --restart\t\t Startet die Anwendung"   
-  echo -e " -h --stop --halt\t\t Beendet die Anwendung"   
-  echo -e " -r --remove --delete\t\t Deinstalliert die Anwendung (Volumes bleiben erhalten!)"   
+function help {
+  echo "01_proxy\n"
+  echo "Installiert die Grundfunktionalität\n\n"
+  echo "Die Funktion bitte mit einem der folgenden Parameter aufrufen:\n"
+  echo -e " -i, --install\t\t\t Installiert die Anwendung"
+  echo -e " -s, --start, --restart\t\t\t Startet die Anwendung"   
+  echo -e " -h, --stop, --halt\t\t\t Beendet die Anwendung"   
+  echo -e " -r, --remove, --delete\t\t\t Deinstalliert die Anwendung (Volumes bleiben erhalten!)"   
 }
 
 case $1 in
