9757d4cb7d7a2b9d74b7ef06649fd28ec292e328
buerodigital <77230151+buerodigital@users.noreply.github.com>
2021-04-29 22:45:42 +0200

    Update _template.sh

--- install/_template.sh
+++ install/_template.sh
@@ -70,17 +70,114 @@ docker-compose -f 01_proxy.yml down
 sudo mkdir /var/lib/docker/volumes/conf_01_proxy/_data/nginx/proxy-confs
 
   cat > default <<EOL
-      
+#Zeilen eingefügt
+map $http_upgrade $connection_upgrade {
+        default upgrade;
+        '' close;
+    }
+
+server {
+    listen 80 default_server;
+
+    server_name _;
+
+    return 301 https://$host$request_uri;
+}
+
+server {
+
+        listen 443 ssl;
+
+        root /config/www;
+        index index.html index.htm index.php;
+
+        server_name _;
+
+
+        #Zeile eingefügt
+        # enable subfolder method reverse proxy confs
+        include /config/nginx/proxy-confs/*.subfolder.conf;
+
+        ssl_certificate /config/keys/cert.crt;
+        ssl_certificate_key /config/keys/cert.key;
+
+        client_max_body_size 0;
+
+#       location / {
+#               try_files $uri $uri/ /index.html /index.php?$args =404;
+#       }
+
+
+        location ~ \.php$ {
+                fastcgi_split_path_info ^(.+\.php)(/.+)$;
+                # With php5-cgi alone:
+                fastcgi_pass 127.0.0.1:9000;
+                # With php5-fpm:
+                #fastcgi_pass unix:/var/run/php5-fpm.sock;
+                fastcgi_index index.php;
+                include /etc/nginx/fastcgi_params;
+
+        }
+}      
   EOL
 sudo mv default /var/lib/docker/volumes/conf_01_proxy/_data/nginx/site-confs/default
 
   cat > proxy.conf <<EOL
-      
+# Timeout if the real server is dead
+proxy_next_upstream error timeout invalid_header http_500 http_502 http_503;
+
+# Proxy Connection Settings
+proxy_buffers 32 4k;
+proxy_connect_timeout 240;
+proxy_headers_hash_bucket_size 128;
+proxy_headers_hash_max_size 1024;
+proxy_http_version 1.1;
+proxy_read_timeout 240;
+proxy_redirect  http://  $scheme://;
+proxy_send_timeout 240;
+
+# Proxy Cache and Cookie Settings
+proxy_cache_bypass $cookie_session;
+#proxy_cookie_path / "/; Secure"; # enable at your own risk, may break certain apps
+proxy_no_cache $cookie_session;
+
+# Proxy Header Settings
+proxy_set_header Connection $connection_upgrade;
+proxy_set_header Early-Data $ssl_early_data;
+proxy_set_header Host $host;
+proxy_set_header Upgrade $http_upgrade;
+proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+proxy_set_header X-Forwarded-Host $host;
+proxy_set_header X-Forwarded-Proto https;
+proxy_set_header X-Forwarded-Ssl on;
+proxy_set_header X-Real-IP $remote_addr;      
   EOL
 sudo mv proxy.conf /var/lib/docker/volumes/conf_01_proxy/_data/nginx/proxy.conf
 
   cat > heimdall.subfolder.conf <<EOL
-      
+## Version 2020/12/09
+# In order to use this location block you need to edit the default file one folder up and comment out the / location
+
+location / {
+    # enable the next two lines for http auth
+    #auth_basic "Restricted";
+    #auth_basic_user_file /config/nginx/.htpasswd;
+
+    # enable the next two lines for ldap auth, also customize and enable ldap.conf in the default conf
+    #auth_request /auth;
+    #error_page 401 =200 /ldaplogin;
+
+    # enable for Authelia, also enable authelia-server.conf in the default site config
+    #include /config/nginx/authelia-location.conf;
+
+    include /config/nginx/proxy.conf;
+    resolver 127.0.0.11 valid=30s;
+    set $upstream_app heimdall;
+    set $upstream_port 443;
+    set $upstream_proto https;
+    proxy_pass $upstream_proto://$upstream_app:$upstream_port;
+
+}      
   EOL
 sudo mv heimdall.subfolder.conf /var/lib/docker/volumes/conf_01_proxy/_data/nginx/proxy-confs/heimdall.subfolder.conf
   
